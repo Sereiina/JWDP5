f6a70016b00bfead582efd3dc4f6563633c37f20
Sereina <amandamoulin731@gmail.com>
2021-08-09 19:52:23 +0200

    refacto + list lenses + onclick for cookie

--- front/js/requestArt.js
+++ front/js/requestArt.js
@@ -2,39 +2,53 @@ let elt = document.getElementById('wrapper-page-art');
 let params = new URLSearchParams(document.location.search.substring(1));
 let id = params.get("id");
 
-function artDescrip(data) {
+function lensesOption(lensesData) {
+  let lenses ="";
+  lensesData.forEach(data => {
+    lenses += `<option value="lentille">`+data+`</option>`
+  });
+  return lenses;
+}
 
-    let artData = `
+function articleConstructor(article) { 
+
+    let articleLenses = lensesOption(article.lenses);
+   
+    let articleInformation = `
     <div>
-    <img class="img-page-art img-article" src="`+data.imageUrl+`">
+    <img class="img-page-art img-article" src="`+article.imageUrl+`">
     </div
 
     <div class="descrip-page-art">
-        <h2>`+data.name+`</h2>
-        <p class="price-article">`+data.price+`€</p>
-        <p class="descrip-article">`+data.description+`</p>
+        <h2>`+article.name+`</h2>
+        <p class="price-article">`+article.price+`€</p>
+        <p class="descrip-article">`+article.description+`</p>
     </div>
 
     <div class="wrap-oranj-button">
-    <div class="oranj-button">Ajouter au panier</div>
+    <button onclick="addToCart('`+article._id+`')" class="oranj-button">Ajouter au panier</button>
     </div>
 
     <div class="list-option-page-art">
-        <!--list from JS-->
+        <label for="lenses-select">Choisir une lentille: </label>
+        <select name="lenses">
+        `+articleLenses+` 
+        
     </div>
     `;
-    console.log(data);
-    const artNode = document.createElement('div');
-      artNode.classList.add('art-sell');
-      artNode.innerHTML=artData;
-      elt.appendChild(artNode);
+    console.log(article);
+    const articleNode = document.createElement('div');
+      articleNode.classList.add('art-sell');
+      articleNode.innerHTML=articleInformation;
+      elt.appendChild(articleNode);
 }
+
 
 
 fetch("http://127.0.0.1:3000/api/cameras/"+id).then(function (response) { //recup les données au bon endroit
   if (response.ok) {
     response.json().then(function (value) {
-     artDescrip(value);
+     articleConstructor(value);
     });
   }
 });
