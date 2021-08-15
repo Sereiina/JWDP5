09a7ae9741d8ac1a62c4cf97de2b8cff6de056f9
Sereina <amandamoulin731@gmail.com>
2021-08-15 17:59:08 +0200

    formatage affichage du prix

--- front/js/request.js
+++ front/js/request.js
@@ -3,21 +3,21 @@ let elt = document.getElementById('wrap-articles');
 function createFrame(data) {
 
   let frame = `
-<div class="wrapper-art">
-<a href="pages-html/page-article.html?id=`+data._id+`">
-<div class="wrap-img-art">
-<img class="img-article" src="`+data.imageUrl+`">
-</div>
+    <div class="wrapper-art">
+        <a href="pages-html/page-article.html?id=`+data._id+`">
+    <div class="wrap-img-art">
+        <img class="img-article" src="`+data.imageUrl+`">
+    </div>
 
-<div class="wrap-des-art">
-<h2 class="name-article">`+data.name+`</h2>
-<p class="price-article">`+data.price+`€</p>
-<p class="descrip-article">`+data.description+`</p>
-</div>
+    <div class="wrap-des-art">
+        <h2 class="name-article">`+data.name+`</h2>
+        <p class="price-article">`+new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR'}).format(data.price/100)+`</p>
+        <p class="descrip-article">`+data.description+`</p>
+    </div>
 
-<!--Recup from JS-->
-</a>
-</div>
+    <!--Recup from JS-->
+        </a>
+    </div>
 `;
  console.log(data);
   const frameNode = document.createElement('a');
